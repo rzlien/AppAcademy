d537ac93309ccefbd2f15e7cdb58926ac7975845
Rachel Lien <rzlien987@gmail.com>
2020-06-22 07:42:05 -0700

    uploaded methods

--- advanced_methods_exercise/01_coprime.rb
+++ advanced_methods_exercise/01_coprime.rb
@@ -3,7 +3,45 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 
+def coprime?(num_1, num_2)
+    factors_1 = factor(num_1)
+    factors_2 = factor(num_2)
 
+    factors_1.each do |i|
+        factors_2.each do |j|
+            return false if i==j
+        end
+    end
+
+    return true
+end
+
+
+def factor(num)
+    factors = []
+
+    (2..num).each do |i|
+        if num%i==0
+            factors << i
+        end
+    end
+
+    return factors
+end
+
+#test for factor
+#print factor(1)
+#puts
+#print factor(0)
+#puts
+#print factor(9)
+#puts
+#print factor(24)
+#puts
+#print factor(6)
+#puts
+
+#test for coprime?
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
 p coprime?(30, 9)     # => false
